238a364569a75c52fa873369401f8463a4df11c6
ChaosHead <ban0kai@gmail.com>
2022-05-08 15:36:56 +0200

    Facebook dropdown menu

--- example-app/src/App.js
+++ example-app/src/App.js
@@ -4,10 +4,13 @@ import { Button } from './components/button'
 import {CardApp} from './components/gitCards'
 import {GameApp} from './components/starMatchGame'
 
+import {FacebookMenu} from './menu/facebookDropdownMenu/facebookDropdownMenu'
+import {SidebarNavigationMenu} from './menu/sidebarNavigationMenu'
+
 function App() {
     return (
     <div className = "App" >
-        <GameApp />
+        <SidebarNavigationMenu />
     </div>
     );
 }
